2a6cceaad26ca76a65c603a430441edec476729b
Corey Larson <coreyleelarson@gmail.com>
2016-12-10 19:24:44 -0600

    Adds lint script and fixes lint/test issues.

--- src/Typekit.spec.js
+++ src/Typekit.spec.js
@@ -1,3 +1,4 @@
+/* eslint no-underscore-dangle: 0 */
 import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
@@ -6,9 +7,9 @@ import buildScript from './utilities/buildScript';
 
 describe('<Typekit />', () => {
   it('should output typekit script when id prop is present', () => {
-    const id = "abc123";
+    const id = 'abc123';
     const wrapper = shallow(<Typekit id={id} />);
-    expect(wrapper.find('script').node.props['dangerouslySetInnerHTML']['__html']).to.equal(buildScript(id));
+    expect(wrapper.find('script').node.props.dangerouslySetInnerHTML.__html).to.equal(buildScript(id));
   });
 
   it('should output nothing when id prop is not present', () => {
