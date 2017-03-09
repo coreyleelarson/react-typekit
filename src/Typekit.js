bcbfb110049252baa3c4eec134617a736b192682
Corey Larson <coreyleelarson@gmail.com>
2017-03-09 17:12:38 -0600

    Removes async as it does not affect inline scripts.

--- src/Typekit.js
+++ src/Typekit.js
@@ -1,45 +1,26 @@
 import includes from 'lodash/includes';
 import split from 'lodash/split';
-import merge from 'lodash/merge';
 import React, { Component, PropTypes } from 'react';
 import buildScript from './utilities/buildScript';
 
 class Typekit extends Component {
-  constructor() {
-    super();
-    this.scriptAttributes = this.scriptAttributes.bind(this);
-  }
-
   componentDidMount() {
     // If typekit has not yet been executed, force it.
     const htmlClasses = document.querySelector('html').className;
     const isLoaded = includes(split(htmlClasses, ' '), 'wf-loading', 'wf-active', 'wf-inactive');
     if (this.script && !isLoaded) {
-      const scriptAttributes = this.scriptAttributes();
+      const { kitId } = this.props;
       const script = document.createElement('script');
-      merge(script, scriptAttributes.props);
-      script.innerHTML = scriptAttributes.innerHTML;
+      script.innerHTML = buildScript(kitId);
       this.script.parentNode.replaceChild(script, this.script);
     }
   }
 
-  scriptAttributes() {
-    const { kitId, async = false } = this.props;
-    return {
-      props: {
-        async,
-        className: 'react-typekit',
-      },
-      innerHTML: buildScript(kitId)
-    };
-  }
-
   render() {
-    const scriptAttributes = this.scriptAttributes();
+    const { kitId } = this.props;
     return this.props.kitId ? (
       <script
-        {...scriptAttributes.props}
-        dangerouslySetInnerHTML={{ __html: scriptAttributes.innerHTML }}
+        dangerouslySetInnerHTML={{ __html: buildScript(kitId) }}
         ref={ref => this.script = ref}
       />
     ) : false;
@@ -47,7 +28,6 @@ class Typekit extends Component {
 }
 
 Typekit.propTypes = {
-  async: PropTypes.bool,
   kitId: PropTypes.string,
 };
 
